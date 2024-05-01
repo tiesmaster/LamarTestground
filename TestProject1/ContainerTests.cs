fdb0cb9a5277f34cb392a347df9e1a74122e6933
Thijs Brobbel <thijs.brobbel@vicompany.nl>
2024-05-01 14:13:11 +0200

    Add container tests

---
+++ TestProject1/ContainerTests.cs
@@ -0,0 +1,18 @@
+﻿using Lamar;
+
+using LamarTestground;
+
+namespace TestProject1;
+
+public class ContainerTests
+{
+    [Fact]
+    public void AssertConfigurationIsValid()
+    {
+        // arrange
+        var container = new Container(new LamarRegistry());
+
+        // act && assert
+        container.AssertConfigurationIsValid();
+    }
+}
