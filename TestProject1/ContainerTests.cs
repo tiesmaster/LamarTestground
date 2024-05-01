24e20fcc3e93fd63a30e1bde2f10de1aebd4855b
Thijs Brobbel <thijs.brobbel@vicompany.nl>
2024-05-01 14:15:23 +0200

    Get the tests to pass!!

--- TestProject1/ContainerTests.cs
+++ TestProject1/ContainerTests.cs
@@ -1,6 +1,15 @@
 ﻿using Lamar;
+using Lamar.IoC.Instances;
 
 using LamarTestground;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+using NSubstitute;
+
+using static Lamar.ServiceRegistry;
 
 namespace TestProject1;
 
@@ -10,9 +19,34 @@ public class ContainerTests
     public void AssertConfigurationIsValid()
     {
         // arrange
-        var container = new Container(new LamarRegistry());
+        var container = new Container(registry =>
+        {
+            registry.IncludeRegistry<LamarRegistry>();
+
+            registry.IncludeRegistry<KnownAspNetCoreDependencies>();
+        });
 
         // act && assert
         container.AssertConfigurationIsValid();
     }
+}
+
+public class KnownAspNetCoreDependencies : ServiceRegistry
+{
+    public KnownAspNetCoreDependencies()
+    {
+        For<IConfiguration>().UseNSubstitute();
+        For<IHttpClientFactory>().UseNSubstitute();
+        For<IHttpContextAccessor>().UseNSubstitute();
+        For<IOptions<TestOptions>>().UseNSubstitute();
+
+        //For(typeof(IOptions<>)).Use(new NullInstance(typeof(IOptions<>)));
+    }
+}
+
+public static class LamarExtensions
+{
+    public static ObjectInstance UseNSubstitute<TService>(this InstanceExpression<TService> serviceRegistration)
+        where TService : class
+        => serviceRegistration.Use(Substitute.For<TService>());
 }
