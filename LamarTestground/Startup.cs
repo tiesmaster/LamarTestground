17d5e29aa594a92750b4be32d7eba2ed6b08aae5
Thijs Brobbel <thijs.brobbel@vicompany.nl>
2024-05-01 14:01:07 +0200

    Wire up options

--- LamarTestground/Startup.cs
+++ LamarTestground/Startup.cs
@@ -2,11 +2,21 @@
 
 public class Startup
 {
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddRazorPages();
 
         services.AddTransient<TestClass>();
+
+        services.Configure<TestOptions>(Configuration);
+
     }
 
     public void Configure(IApplicationBuilder app)
